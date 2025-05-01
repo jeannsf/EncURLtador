fa7f025d50fadc222adf6e3b4fb1b79aa65274c6
Jean Freitas <jeannsfreitas@gmail.com>
2025-05-01 20:46:40 -0300

    feat: add useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ShortUrlModule } from './short-url/short-url.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ShortUrlModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
