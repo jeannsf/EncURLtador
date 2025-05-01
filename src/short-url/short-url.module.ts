3ae936aadd809eb489aa1cd85295e465124c1b79
Jean Freitas <jeannsfreitas@gmail.com>
2025-05-01 20:46:55 -0300

    feat: add usersModule

--- src/short-url/short-url.module.ts
+++ src/short-url/short-url.module.ts
@@ -3,10 +3,18 @@ import { ShortUrlService } from './short-url.service';
 import { ShortUrlController } from './short-url.controller';
 import { ShortUrlRepository } from './short-url.repository';
 import { PrismaModule } from 'prisma/prisma.module';
+import { UsersController } from 'src/users/users.controller';
+import { UserRepository } from 'src/users/user.repository';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ShortUrlController],
-  providers: [ShortUrlService, ShortUrlRepository],
+  controllers: [ShortUrlController, UsersController],
+  providers: [
+    ShortUrlService,
+    ShortUrlRepository,
+    UserRepository,
+    UsersService,
+  ],
 })
 export class ShortUrlModule {}
